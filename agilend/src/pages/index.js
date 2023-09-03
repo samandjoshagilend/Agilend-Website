0233b576d194dfdac438109e904f524f3d92adbd
Josh Stroud <josh@agilend.net>
2023-09-03 12:05:54 -0700

    Add Gatsby hello world

---
+++ agilend/src/pages/index.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <div>
+      <div>
+        <h1>Agilend</h1>
+        <h2>A startup for AI.</h2>
+      </div>
+      <footer>
+        <div>info@agilend.net</div>
+      </footer>
+    </div>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
